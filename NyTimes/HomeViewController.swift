4089a92f18b5e162351624f54f99f10eee3d52d3
Sumit Kumar <SK00560353@techmahindra.com>
2020-08-14 19:17:54 +0530

    Basic project setup with skeleton UI

---
+++ NyTimes/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NyTimes
+//
+//  Created by Sumit Kumar on 14/08/20.
+//  Copyright © 2020 sumitkr110. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
